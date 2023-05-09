ad7381b5f6d395bd40fba28968b887cb79c69de8
wkeitz <92413262+wkeitz@users.noreply.github.com>
2023-05-09 14:31:27 -0700

    Update usart.c

--- Source Code/usart.c
+++ Source Code/usart.c
@@ -36,3 +36,36 @@ uint8_t USART_read(void){
 	uint8_t input = USART2->RDR;
 	return input;
 }
+
+void USART_string(char *str){
+	while(*str){
+		USART_write(*str);
+		str++;
+	}
+}
+
+void USART_ESC_code(char *str){
+	//ESC
+	USART_write(0x1B);
+	//[
+	USART_write(0x5B);
+	while(*str){
+		USART_write(*str);
+		str++;
+	}
+}
+
+void USART_print_sample(uint16_t sample){
+	//take a 4 digit variable (decimal) and write to TDR
+	char digit_0, digit_1, digit_2, digit_3;
+	// Split each digit in sample value into individual values
+	digit_0 = sample / 1000;
+	digit_1 = (sample / 100) - (10* digit_0);
+	digit_2 = (sample / 10) - (100* digit_0) - (10 * digit_1);
+	digit_3 = sample - (1000 * digit_0) - (100 * digit_1) - (10 * digit_2);
+	// Print each individual digit to the terminal
+	USART_write(digit_0 + 0x30);
+	USART_write(digit_1 + 0x30);
+	USART_write(digit_2 + 0x30);
+	USART_write(digit_3 + 0x30);
+}
